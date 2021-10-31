ca6eea2c993adae430ccefceda34dfbfa31e9fb7
Reynelbis Larquin Guerra <reynelbis.larquin@reduc.edu.cu>
2021-10-30 21:47:25 -0400

    se agrego el cerrado del programa para cuando termine la accion escogida

--- bin/global.js
+++ bin/global.js
@@ -6,42 +6,43 @@
 // Recuperar el primer argumento
 // var name = args[0];
 
-var myLibrary = require("../lib/index");
-const { menu } = require("../lib/menu");
-const { dtos } = require("../lib/dtos");
-const { service } = require("../lib/service");
-const { repository } = require("../lib/repository");
-const { controller } = require("../lib/controller");
+var {estructura, queryParams, msn} = require("../lib");
+const {menu} = require("../lib/menu");
+const {dtos} = require("../lib/dtos");
+const {service} = require("../lib/service");
+const {repository} = require("../lib/repository");
+const {controller} = require("../lib/controller");
 
 // Crea la estructura de carpetas.
 (async () => {
-  myLibrary.msn("NESTOOL-CLI");
+    msn("NESTOOL-CLI");
 
-  let opt = await menu();
-  switch (opt.opcion) {
-    case "modulo":
-      myLibrary.estructura(await myLibrary.queryParams());
-      break;
+    let opt = await menu();
+    switch (opt.opcion) {
+        case "modulo":
+            await estructura(queryParams());
+            break;
 
-    case "controlador":
-      controller();
-      break;
+        case "controlador":
+            await controller();
+            break;
 
-    case "dtos":
-      dtos();
-      break;
+        case "dtos":
+            await dtos();
+            break;
 
-    case "service":
-      service();
-      break;
-    case "mapper":
-      break;
-    case "repository":
-      repository();
-      break;
-    case "entity":
-      break;
-    case "crud":
-      break;
-  }
+        case "service":
+            await service();
+            break;
+        case "mapper":
+            break;
+        case "repository":
+            await repository();
+            break;
+        case "entity":
+            break;
+        case "crud":
+            break;
+    }
+    process.exit(1);
 })();
